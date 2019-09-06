3d48170725fb09bb33fbcd32cf31176081d7fad5
Fu Chen <cfmcgrady@gmail.com>
2019-09-06 10:25:41 +0800

    refactoring CreateMLFlowFunctionCommand

--- core/src/main/scala/org/apache/spark/sql/execution/command/CreateMLFlowFunctionCommand.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/command/CreateMLFlowFunctionCommand.scala
@@ -1,7 +1,8 @@
 package org.apache.spark.sql.execution.command
-import org.apache.spark.sql.panda.PandasFunctionManager
-import org.apache.spark.sql.types.{DataType, DataTypes}
+
 import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.panda.PandasFunctionManager
+import org.apache.spark.sql.types.DataType
 
 /**
  * @time 2019-09-05 13:52
@@ -30,54 +31,3 @@ case class CreateMLFlowFunctionCommand(
     Seq.empty[Row]
   }
 }
-
-
-//case class CreateFunctionCommand(
-//                                  databaseName: Option[String],
-//                                  functionName: String,
-//                                  className: String,
-//                                  resources: Seq[FunctionResource],
-//                                  isTemp: Boolean,
-//                                  ignoreIfExists: Boolean,
-//                                  replace: Boolean)
-//  extends RunnableCommand {
-//
-//  if (ignoreIfExists && replace) {
-//    throw new AnalysisException("CREATE FUNCTION with both IF NOT EXISTS and REPLACE" +
-//      " is not allowed.")
-//  }
-//
-//  // Disallow to define a temporary function with `IF NOT EXISTS`
-//  if (ignoreIfExists && isTemp) {
-//    throw new AnalysisException(
-//      "It is not allowed to define a TEMPORARY function with IF NOT EXISTS.")
-//  }
-//
-//  // Temporary function names should not contain database prefix like "database.function"
-//  if (databaseName.isDefined && isTemp) {
-//    throw new AnalysisException(s"Specifying a database in CREATE TEMPORARY FUNCTION " +
-//      s"is not allowed: '${databaseName.get}'")
-//  }
-//
-//  override def run(sparkSession: SparkSession): Seq[Row] = {
-//    val catalog = sparkSession.sessionState.catalog
-//    val func = CatalogFunction(FunctionIdentifier(functionName, databaseName), className, resources)
-//    if (isTemp) {
-//      // We first load resources and then put the builder in the function registry.
-//      catalog.loadFunctionResources(resources)
-//      catalog.registerFunction(func, overrideIfExists = replace)
-//    } else {
-//      // Handles `CREATE OR REPLACE FUNCTION AS ... USING ...`
-//      if (replace && catalog.functionExists(func.identifier)) {
-//        // alter the function in the metastore
-//        catalog.alterFunction(func)
-//      } else {
-//        // For a permanent, we will store the metadata into underlying external catalog.
-//        // This function will be loaded into the FunctionRegistry when a query uses it.
-//        // We do not load it into FunctionRegistry right now.
-//        catalog.createFunction(func, ignoreIfExists)
-//      }
-//    }
-//    Seq.empty[Row]
-//  }
-//}
