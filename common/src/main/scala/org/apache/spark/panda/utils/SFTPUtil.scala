e49d6eba86fd1a7469cdbfc7078b1b823c517c84
Fu Chen <cfmcgrady@gmail.com>
2019-09-06 15:09:49 +0800

    handle exception in SFTPUtil.get()

--- common/src/main/scala/org/apache/spark/panda/utils/SFTPUtil.scala
+++ common/src/main/scala/org/apache/spark/panda/utils/SFTPUtil.scala
@@ -1,7 +1,10 @@
 package org.apache.spark.panda.utils
 
+import java.io.IOException
+
 import com.jcraft.jsch.OpenSSHConfig
 import net.schmizz.sshj.SSHClient
+import net.schmizz.sshj.sftp.SFTPClient
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier
 import net.schmizz.sshj.xfer.FileSystemFile
 
@@ -18,6 +21,7 @@ object SFTPUtil {
    * @param remoteFile the file or directory of the remote host, notice that default root path is user home.
    * @param localDirectory the local directory prefix that we put the downloaded file.
    */
+  @throws(classOf[IOException])
   def download(host: String,
                remoteFile: String,
                localDirectory: String): Unit = {
@@ -25,21 +29,27 @@ object SFTPUtil {
     val userHomePath = System.getProperty("user.home")
     val configs = OpenSSHConfig.parseFile(s"${userHomePath}/.ssh/config")
     val ssh = new SSHClient()
+    var sftp: SFTPClient = null
     ssh.loadKnownHosts()
     ssh.addHostKeyVerifier(new PromiscuousVerifier())
 
-//    val path = "/Users/fchen/.ssh/dxy"
     val config = configs.getConfig(host)
     val key = ssh.loadKeys(config.getValue("IdentityFile").replaceFirst("~", userHomePath))
     val hostname = config.getHostname
     val port = config.getPort
-    ssh.connect(hostname, port)
-    ssh.authPublickey(config.getUser, key)
-    val sftp = ssh.newSFTPClient()
-    sftp.get(remoteFile, new FileSystemFile(localDirectory))
-    sftp.close()
-    ssh.disconnect()
-    ssh.close()
+    try {
+        ssh.connect(hostname, port)
+        ssh.authPublickey(config.getUser, key)
+        sftp = ssh.newSFTPClient()
+        sftp.get(remoteFile, new FileSystemFile(localDirectory))
+    } finally {
+      if (sftp != null) {
+        sftp.close()
+      }
+      if (ssh.isConnected) {
+        ssh.disconnect()
+      }
+      ssh.close()
+    }
   }
-
 }
